b9814a8c3beb63693f2e445467e81b5c719cc1b8
Anton Zmiievskyi <anton.zmievsky@gmail.com>
2022-11-19 16:25:57 +0200

    fixed

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -2,27 +2,26 @@ import PropTypes from "prop-types";
 import Style from "./Statistics.module.scss";
 
 export default function Statistics({ title, stats }) {
-    return (
-      <section className={Style.statistics}>
-        <h2 className={Style.title}>{title}</h2>
+  return (
+    <section className={Style.statistics}>
+      {title && <h2 className={Style.title}>{title}</h2>}
 
-        <ul className={Style.list}>
-          {stats.map((stat) => {
-            return (
-              <li key={stat.id} className={Style.item}>
-                <span className={Style.label}>{stat.label}</span>
-                <span className={Style.percentage}>{stat.percentage}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    );
+      <ul className={Style.list}>
+        {stats.map((stat) => {
+          return (
+            <li key={stat.id} className={Style.item}>
+              <span className={Style.label}>{stat.label}</span>
+              <span className={Style.percentage}>{stat.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
 
-
 Statistics.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
